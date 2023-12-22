2808ecfa55d7d62633fdef5ede8daed68b1eae10
QuocHuy248 <quochuy248@gmail.com>
2023-12-22 08:26:20 +0700

    formmik

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import FormMik from "./components/FormMik";
 
 function App() {
     return <div className="Container">
-      
+      <FormMik/>
     </div>;
 }
 
