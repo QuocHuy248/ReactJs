350d328aa4d4f4ed23b0a1f84d6bac62c0bc5dc8
QuocHuy248 <quochuy248@gmail.com>
2023-12-22 10:33:19 +0700

    timefunction

--- hook/src/components/Lesson.jsx
+++ hook/src/components/Lesson.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Lesson() {
-  return (
-    <div>Lesson</div>
-  )
+    const [count, setCount] = useState(50);
+    // let timerId;
+
+    // const handleStart = () => {
+    //     timerId = setInterval(() => setCount(count - 1), 1000);
+    //     console.log("Start ", timerId);
+    // };
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setCount((prev) => prev - 1);
+            console.log("countdown.");
+        }, 1000);
+        return () => clearInterval(timerId);
+    }, []);
+
+    // const handleStop = () => {
+    //     clearInterval(timerId);
+    //     console.log("Stop", timerId);
+    // };
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            {/* <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button> */}
+        </div>
+    );
 }
