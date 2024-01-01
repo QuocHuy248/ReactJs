ceeb93ba8c500692ded2ac0eff60b6c3c9c30fd6
QuocHuy248 <quochuy248@gmail.com>
2024-01-01 22:44:41 +0700

    useRef update

--- hook/src/components/Lesson.jsx
+++ hook/src/components/Lesson.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Lesson() {
     const [count, setCount] = useState(50);
-    // let timerId;
-
-    // const handleStart = () => {
-    //     timerId = setInterval(() => setCount(count - 1), 1000);
-    //     console.log("Start ", timerId);
-    // };
+    let timerId = useRef();
+    let prevCount = useRef();
+    const h1Ref = useRef();
     useEffect(() => {
-        const timerId = setInterval(() => {
+        prevCount.current = count;
+        console.log("count In useEffect", count);
+    }, [count]);
+    useEffect(() => {
+        console.log(h1Ref.current.getBoundingClientRect());
+    });
+    const handleStart = () => {
+        timerId.current = setInterval(() => {
             setCount((prev) => prev - 1);
-            console.log("countdown.");
+            console.log("count in call back", count);
         }, 1000);
-        return () => clearInterval(timerId);
-    }, []);
+        // console.log("Start ", timerId.current);
+    };
+    console.log(count, prevCount.current);
 
-    // const handleStop = () => {
-    //     clearInterval(timerId);
-    //     console.log("Stop", timerId);
-    // };
+    const handleStop = () => {
+        clearInterval(timerId.current);
+        console.log("Stop", timerId.current);
+    };
 
     return (
         <div>
-            <h1>{count}</h1>
-            {/* <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button> */}
+            <h1 ref={h1Ref}>{count}</h1>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
         </div>
     );
 }
